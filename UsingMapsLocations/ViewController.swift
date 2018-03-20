ceec536369b19cbd1287e516b653eb908be395ae
Juan Jose Colin <juan.jose.colin@gmail.com>
2018-03-19 20:19:49 -0600

    Initial Commit

---
+++ UsingMapsLocations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UsingMapsLocations
+//
+//  Created by Juan Jose Colin on 3/19/18.
+//  Copyright © 2018 Juan Jose Colin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
